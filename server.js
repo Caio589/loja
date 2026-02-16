7be84b816ea961d1e6ed3d6c1b50655286440bfc
C.O Sistemas <caiosan321@icloud.com>
2026-02-15 23:58:17 -0300

    Implement produtos GET and POST endpoints
    Added endpoints for managing produtos.

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const pix = require("./pix");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/pix", pix);
+let produtos = [];
+
+app.get("/produtos", (req, res) => {
+  res.json(produtos);
+});
+
+app.post("/produtos", (req, res) => {
+  produtos.push(req.body);
+  res.sendStatus(201);
+});
 
 app.listen(3000, () =>
-  console.log("Servidor Pix rodando 🚀")
+  console.log("Servidor rodando 🚀")
 );
